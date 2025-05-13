b4b81ca3ccb2030c8d8e08f6c297171cfb607d45
deividviali <deividviali@gmail.com>
2025-05-13 10:29:47 -0400

    Comentado o código

--- app.py
+++ app.py
@@ -24,10 +24,10 @@ app.secret_key = 'troque_para_uma_chave_secreta_segura'
 ATIVIDADES_OPCOES = {
     '1': ('A', 'Atividade A'),
     '2': ('B', 'Atividade B'),
-    '3': ('C', 'Atividade C'),
-    '4': ('D', 'Atividade D'),
-    '5': ('E', 'Atividade E'),
-    '6': ('F', 'Atividade F'),
+    # '3': ('C', 'Atividade C'),
+    # '4': ('D', 'Atividade D'),
+    # '5': ('E', 'Atividade E'),
+    # '6': ('F', 'Atividade F'),
     #'7': ('PPE', 'Prova Prática de Execução'),  
      '8': ('CFS25', 'Atividade Prática CFS2025'),  
     
